95822867cc8adb1aab986598cc8dcaf7d9cb1a0e
bustosdaniel <bustosneifer@gmail.com>
2022-05-10 15:27:08 -0500

    añadiendo react y empezando proyecto

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { getActiveElement } from '@testing-library/user-event/dist/utils';
+
+const element = document.getElementById('root')
+
+const root = createRoot(element);
+root.render(<App/>);
