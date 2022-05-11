f4efcf238e2ccb5a68126960a7e3f68b9c0bc7e1
bustosdaniel <bustosneifer@gmail.com>
2022-05-11 16:31:22 -0500

    añadiendo carpetas y localstorage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { getActiveElement } from '@testing-library/user-event/dist/utils';
+import App from './App/index.js';
+//import { getActiveElement } from '@testing-library/user-event/dist/utils';
 
 const element = document.getElementById('root')
 
